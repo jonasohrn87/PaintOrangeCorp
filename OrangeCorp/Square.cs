5953f2471e1214ec2b360ffba537bce05f9ef1bb
Jonas Öhrn <theeagle87@hotmail.com>
2024-03-15 13:19:40 +0100

    fixed a coloring palet, and a image box for chosen color

---
+++ OrangeCorp/Square.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrangeCorp
+{
+    public class Square : Shape
+    {
+        public int side { get; set; }
+    }
+}
